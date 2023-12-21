f842d3b3f053cf3e3e295a6ae1ada481fb073b9d
Bruno de Moura <bruno.moura@ziyon.fr>
2023-12-21 13:08:03 -0300

    challenge concluded

--- index.js
+++ index.js
@@ -2,9 +2,41 @@ let nameHero = "Bruno"
 let xpHero = 0
 let levelHero = ""
 
-switch(xpHero) {
+for (let xpHero = 0; xpHero <= 10500; xpHero += 500) {
+switch(true) {
     case xpHero < 1000:
     levelHero = "Ferro"
-    console.log("O Herói de nome " + nameHero +" está no nível de "+ levelHero)
     break
+
+    case xpHero >= 1001 && xpHero <= 2000:
+    levelHero = "Bronze"
+    break
+
+    case xpHero >= 2001 && xpHero <= 5000:
+    levelHero = "Prata"
+    break
+
+    case xpHero >= 5001 && xpHero <= 7000:
+    levelHero = "Ouro"
+    break
+
+    case xpHero >= 7001 && xpHero <= 8000:
+    levelHero = "Platina"
+    break
+
+    case xpHero >= 8001 && xpHero <= 9000:
+    levelHero = "Ascendente"
+    break
+
+    case xpHero >= 9001 && xpHero <= 10000:
+    levelHero = "Imortal"
+    break
+
+    case xpHero >= 10001:
+    levelHero = "Radiante"
+    break
+}
+
+let message = "O Herói de nome " + nameHero +" está no nível de "+ levelHero
+console.log(message)
 }
