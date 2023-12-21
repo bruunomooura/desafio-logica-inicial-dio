108b3f5935d06b8356dd185ab16b38121516c74b
Bruno de Moura <124161514+bruunomooura@users.noreply.github.com>
2023-12-21 12:11:14 -0300

    Inicio da resolução do desafio.

---
+++ index.js
@@ -0,0 +1,10 @@
+let nameHero = "Bruno"
+let xpHero = 0
+let levelHero = ""
+
+switch(xpHero) {
+    case xpHero < 1000:
+    levelHero = "Ferro"
+    console.log("O Herói de nome " + nameHero +" está no nível de "+ levelHero)
+    break
+}
